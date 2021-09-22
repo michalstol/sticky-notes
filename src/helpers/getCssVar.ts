bf5ce1717dd7c5a1009303c653568443fb0c4086
Michal <michalstolarski@gmail.com>
2021-09-22 17:11:24 +0200

    covering getCSSVar return

--- src/helpers/getCssVar.ts
+++ src/helpers/getCssVar.ts
@@ -1,4 +1,6 @@
-function getCssVar(computedStyles: CSSStyleDeclaration) {
+function getCssVar(
+    computedStyles: CSSStyleDeclaration
+): (cssVar: string) => string {
     return function (cssVar: string): string {
         return computedStyles.getPropertyValue(cssVar).trim();
     };
